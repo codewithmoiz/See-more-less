fa3dbf935fd7ae5d9e17f816a2e6bb7b7cb04ae5
Abdul Moiz <138908943+codewithmoiz@users.noreply.github.com>
2024-01-15 01:58:03 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var btn = document.querySelector('button');
 var textElement = document.getElementById('text');
 var originalText = textElement.innerText;
 
+if (originalText.length > 50) {
+    textElement.innerText = originalText.slice(0, 50);
+    btn.style.display = 'block';
+}
+
 function showMoreLess() {
     if (textElement.innerText.length > 50) {
         textElement.innerText = textElement.innerText.slice(0, 50);
@@ -10,4 +15,4 @@ function showMoreLess() {
         textElement.innerText = originalText;
         btn.innerText = "See less";
     }
-}
+};
